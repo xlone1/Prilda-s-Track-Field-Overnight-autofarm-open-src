1dc425ae93279285f12f017d8c4fe7957ded4529
xlone1 <81891118+xlone1@users.noreply.github.com>
2022-04-12 21:43:02 -0400

    Add files via upload

---
+++ prilda autofarm.lua
@@ -0,0 +1,41 @@
+-- i reccomend to do this overnight lol
+
+if game.PlaceId == "4883932082" then return end
+
+local CoreGui = game:GetService("StarterGui") --
+
+CoreGui:SetCore("SendNotification", {
+    Title = "xlone#2836",
+    Text = "",
+    Duration = 10, 
+})
+
+while wait(3) do
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint5, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint5, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint1, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint1, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint2, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint2, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint3, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint3, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint4, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint4, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint6, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint6, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint7, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint7, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint8, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint8, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint9, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint9, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint10, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint10, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint11, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Checkpoints.Checkpoint11, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+firetouchinterest(game:GetService("Workspace").Track.Markers.MeterLines.FinishWall, game.Players.LocalPlayer.Character.HumanoidRootPart, 1)
+firetouchinterest(game:GetService("Workspace").Track.Markers.MeterLines.FinishWall, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
+local ts = game:GetService("TweenService")
+local tween = ts:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(2), {CFrame = CFrame.new(-76.5569763, 4.05000019, -46.8654594, 0.999543965, -1.14478844e-07, 0.0301975701, 1.13291534e-07, 1, 4.10291712e-08, -0.0301975701, -3.75893308e-08, 0.999543965)})
+tween:Play()
+end
